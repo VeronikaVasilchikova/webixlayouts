a72d6a3af4a12ab34484de69656772463a9baa78
VeronikaVasilchikova <veroni4ka.v@gmail.com>
2020-02-28 00:50:00 +0300

    Add possibility to validate and clear form and set new data from form to datatable

--- formEditFilmsData.js
+++ formEditFilmsData.js
@@ -1,3 +1,6 @@
+const today = new Date();
+const currentYear = today.getFullYear();
+
 const form_edit_films_data = [
   {
     template:"Edit films",
@@ -6,28 +9,35 @@ const form_edit_films_data = [
   {
     view:"text",
     label:"Title",
-    name:"title"
+    name:"title",
+    required:true,
+    invalidMessage:"Title must be filled in"
   },
   {
     view:"text",
     label:"Year",
-    name:"year"
+    name:"year",
+    //bottomPadding: 25,
+    invalidMessage:`Year should be between 1970 and ${currentYear}`
   },
   {
     view:"text",
     label:"Rating",
-    name:"rating"
+    name:"rating",
+    required:true,
+    invalidMessage:"Rating cannot be empty or 0"
   },
   {
     view:"text",
     label:"Votes",
-    name:"votes"
+    name:"votes",
+    invalidMessage:"Votes must be less than 100000"
   },
   {
     margin:10,
     cols:[
-      {view:"button", value:"Add new", css:"webix_primary", width: 110},
-      {view:"button", value:"Clear", width: 110},
+      {view:"button", id:"btnAddNew", value:"Add new", css:"webix_primary", width: 110},
+      {view:"button", id:"btnClear", value:"Clear", width: 110}
     ]
   },
   {},
